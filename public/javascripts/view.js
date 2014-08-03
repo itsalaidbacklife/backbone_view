376ce87bc9d22180cd5eb96f48a43115e94499c8
itsalaidbacklife <itsalaidbacklife@gmail.com>
2014-08-03 18:24:30 -0400

    Formatted for my punkass friend

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -1,34 +1,34 @@
 (function($) {
 	console.log('attached');
 	var ListView = Backbone.View.extend({
-	  el: $('body'), //el ataches to body
+		el: $('body'), //el ataches to body
 
-	  events: {
-	  'click button#add': 'addItem'
-	  },
+		events: {
+			'click button#add': 'addItem'
+		},
 
-	  intitialize: function() {
-	  	_.bindAll(this, 'render', 'addItem'); // allows render method to refer to the rendered ListView as this
-	  	console.log('initializing');
-	  	this.counter = 0; //total # of items
-	  	this.str='blarg';
-	  	this.render();
-	  },
+		intitialize: function() {
+			_.bindAll(this, 'render', 'addItem'); // allows render method to refer to the rendered ListView as this
+			console.log('initializing');
+			this.counter = 0; //total # of items
+			this.str = 'blarg';
+			this.render();
+		},
 
-	  render: function() {
-	  	console.log('rendering');
-	  	$(this.el).append("<button id='add'>Add List Item</button>");
-	  	$(this.el).append("<ul></ul>");
-	  },
+		render: function() {
+			console.log('rendering');
+			$(this.el).append("<button id='add'>Add List Item</button>");
+			$(this.el).append("<ul></ul>");
+		},
 
-	  addItem: function() {
-	  this.counter++;
-	  $('ul', this.el).append("<li>hello world"+this.counter+"<li>");
-	}
-});
+		addItem: function() {
+			this.counter++;
+			$('ul', this.el).append("<li>hello world" + this.counter + "<li>");
+		}
+	});
 
-console.log('before new var');
-var listView = new ListView();
-console.log('after new var');
-console.log(listView.str);
+	console.log('before new var');
+	var listView = new ListView();
+	console.log('after new var');
+	console.log(listView.str);
 })(jQuery);
